7e7829ced094f81607004d3755e56b9a2cd58b22
desaic <desaic@alum>
2020-12-10 01:39:43 -0500

    entry point

--- exp/kern/bootboot/run_qemu.sh
+++ exp/kern/bootboot/run_qemu.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/sh
-qemu-system-x86_64 -drive file=kern.img,format=raw -serial stdio
+cp ../kern.elf ./boot
+mkbootimg example.json kern.img
+cmd="qemu-system-x86_64 -drive file=kern.img,format=raw -serial stdio"
+echo $cmd
+$cmd
