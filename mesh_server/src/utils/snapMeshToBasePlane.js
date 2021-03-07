e42658b21b322dcf1a7a2a7011740cf90e26abce
desaic <desai@inkbit3d.com>
2021-03-07 01:36:18 -0500

    material for transmitted mesh. snap to z

--- mesh_server/src/utils/snapMeshToBasePlane.js
+++ mesh_server/src/utils/snapMeshToBasePlane.js
@@ -6,6 +6,6 @@ export const snapMeshToBasePlane = (mesh) => {
 	// Moves the mesh up or down based on its bounding box
 	mesh.geometry.center();	// Center the geometry (and origin) about the center of the bounding box
 	var boundBox = new THREE.Box3().setFromObject(mesh);
-	mesh.position.set(0, boundBox.getSize().y / 2, 0);
+	mesh.position.set(0, 0, boundBox.getSize().z / 2);
 	return mesh;
 }
