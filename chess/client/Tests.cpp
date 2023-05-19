8451bf3cc064c4090ee0acf0f316efc8b3a08035
desaic <desai@inkbit3d.com>
2023-05-18 20:40:41 -0400

    test more

--- chess/client/Tests.cpp
+++ chess/client/Tests.cpp
@@ -221,10 +221,12 @@ void TestMovePerft() {
   ChessBoard b;
   b.SetStartPos();
   //b.FromFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
-  b.FromFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+  //b.FromFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+  //b.FromFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
+  b.FromFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
  // PrintMoveCounts();
-  SaveMoveCounts("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 5);
-  int depth =6 ;
+  //SaveMoveCounts("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 5);
+  int depth =5 ;
   std::vector<MoveCounts>moveStats(depth);
   EnumerateMoves(b, depth, moveStats);
   for (size_t i = 0; i < moveStats.size(); i++) {
