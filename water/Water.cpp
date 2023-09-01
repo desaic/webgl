3bcf46be9a768c177ba8846abc9a229267284a15
desaic <desai@inkbit3d.com>
2023-08-31 23:54:37 -0400

    skeleton

--- water/Water.cpp
+++ water/Water.cpp
@@ -1,1 +1,20 @@
 #include "Water.h"
+
+int Water::Allocate(unsigned sx, unsigned sy, unsigned sz) {
+  u.Allocate(sx + 1, sy + 1, sz + 1);
+  phi.Allocate(sx + 1, sy + 1, sz + 1);
+  p.Allocate(sx, sy, sz);
+}
+
+int Water::AdvectU() {}
+int Water::AddBodyForce() {}
+int Water::SolveP() {}
+int Water::AdvectPhi() {}
+
+int Water::Step() {
+  AdvectU();
+  AddBodyForce();
+  SolveP();
+  AdvectPhi();
+  return 0;
+}
--- water/Water.h
+++ water/Water.h
@@ -1,12 +1,27 @@
 #ifndef WATER_H
 #define WATER_H
-#include "Vec3.h"
 #include "Array3D.h"
-class Water{
-  public:
-    int Step(){return 0;}
-  private:
-    Array3D<Vec3f> u;
-	Array3D<float> phi;	
+#include "Vec3.h"
+class Water {
+ public:
+  int Step() { return 0; }
+  int AdvectU();
+  int AddBodyForce();
+  int SolveP();
+  int AdvectPhi();
+
+  int Allocate(unsigned sx, unsigned sy, unsigned sz);
+
+ private:
+  //defined on face centers
+  // size is +1 of voxel grid size.
+  Array3D<Vec3f> u;
+  //defined on vertices
+  //size is +1 of voxel grid size.
+  Array3D<float> phi;
+  //intermediate variable pressure.
+  Array3D<float> p;
+  float dt=0.01f;
+  float h=0.1f;
 };
 #endif
