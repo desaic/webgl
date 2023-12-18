2246bb32ce0e1aa14a3b3f3d1107d61f56c5674a
Desai <dchen@inkbit3d.com>
2023-12-18 16:38:10 -0500

    move util functions to util src files

--- fem/StringUtil.h
+++
@@ -1,7 +0,0 @@
-#ifndef STRING_UTIL_H
-#define STRING_UTIL_H
-#include <string>
-std::string get_suffix(const std::string& str,
-                       const std::string& delimiters = ".");
-std::string remove_suffix(const std::string& str, const std::string& suffix);
-#endif
