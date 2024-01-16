9e91cca09833ace4ca56a752bfa78bdc1074849c
desaic <desai@inkbit3d.com>
2024-01-15 20:47:28 -0500

    group into columns

--- MeshDebugger/MakeHelix.cpp
+++ MeshDebugger/MakeHelix.cpp
@@ -4,12 +4,24 @@
 #include "MakeHelix.h"
 #include  <algorithm>
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="ymin"></param>
+/// <param name="y0"></param>
+/// <param name="y1"></param>
+/// <param name="r"></param>
+/// <param name="divs"></param>
+/// <param name="pitch"></param>
+/// <param name="startingIdx">index of the first vertex included in the helix</param>
+/// <returns></returns>
 std::vector<Vec3f> MakeOneHelix(float ymin, float y0, float y1, float r, unsigned divs,
-                                float pitch) {
+                                float pitch, unsigned & startingIdx) {
   float y = y0;
   float dyPerStep = pitch / divs;
   std::vector<Vec3f> points;
   unsigned vCount = 0;
+  bool started = false;
   while (y < y1) {
     float phase = vCount % divs / float(divs);
     float theta = phase * (2 * M_PI);
@@ -17,6 +29,10 @@ std::vector<Vec3f> MakeOneHelix(float ymin, float y0, float y1, float r, unsigne
     float z = r * std::cos(theta);
     y = y0 + dyPerStep * vCount;
     if (y >= ymin) {
+      if (!started) {
+        startingIdx = vCount;
+        started = true;
+      }
       // only add verts above ground
       points.push_back(Vec3f(x, y, z));
     }
@@ -29,6 +45,23 @@ void AddVerts(const std::vector<Vec3f>& vec, TrigMesh& mesh) {
   for (const auto v : vec) {
     mesh.AddVert(v[0], v[1], v[2]);
   }
+}
+
+struct VertCol {
+  bool hasVert[4] = {};
+  Vec3f verts[4];
+};
+
+std::vector<VertCol> GroupIntoCols(const std::vector<Vec3f>& v0, const std::vector<Vec3f>& v1,
+                   const std::vector<Vec3f>& v2, const std::vector<Vec3f>& v3,
+  const unsigned * startIdx) {
+  size_t numCols = startIdx[3] + v3.size();
+  std::vector<VertCol> cols;
+  for (size_t i = 0; i < numCols; i++) {
+    VertCol col;
+    cols.push_back(col);
+  }
+  return cols;
 }
 
 //   y      v3
@@ -50,17 +83,23 @@ int MakeHelix(const HelixSpec& spec, TrigMesh& mesh) {
   float r0 = spec.inner_diam / 2;
   float r1 = spec.outer_diam / 2;
   float slopeWidth = std::abs(spec.inner_width - spec.outer_width) / 2;
-  std::vector<Vec3f> h0 = MakeOneHelix(ymin, y0, y1, r0, spec.divs, spec.pitch);
+  unsigned startIdx[4] = {};
+  std::vector<Vec3f> h0 = MakeOneHelix(ymin, y0, y1, r0, spec.divs, spec.pitch, startIdx[0]);
   y0 = -spec.inner_width + slopeWidth;
-  std::vector<Vec3f> h1 = MakeOneHelix(ymin, y0, y1, r1, spec.divs, spec.pitch);
+  std::vector<Vec3f> h1 =
+      MakeOneHelix(ymin, y0, y1, r1, spec.divs, spec.pitch, startIdx[1]);
   y0 = -slopeWidth;
-  std::vector<Vec3f> h2 = MakeOneHelix(ymin, y0, y1, r1, spec.divs, spec.pitch);
+  std::vector<Vec3f> h2 =
+      MakeOneHelix(ymin, y0, y1, r1, spec.divs, spec.pitch, startIdx[2]);
   y0 = 0;
-  std::vector<Vec3f> h3 = MakeOneHelix(ymin, y0, y1, r0, spec.divs, spec.pitch);
+  std::vector<Vec3f> h3 =
+      MakeOneHelix(ymin, y0, y1, r0, spec.divs, spec.pitch, startIdx[3]);
   AddVerts(h0, mesh);
   AddVerts(h1, mesh);
   AddVerts(h2, mesh);
   AddVerts(h3, mesh);
+
+  std::vector<VertCol> cols = GroupIntoCols(h0, h1, h2, h3, startIdx);
 
   return 0;
 }
