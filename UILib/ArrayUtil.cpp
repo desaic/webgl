5b6ca03c92ce372e9c1b15c1239d445c6f184468
desai <desaicwtf@gmail>
2024-02-28 18:38:27 -0500

    array util

---
+++ UILib/ArrayUtil.cpp
@@ -0,0 +1,5 @@
+#include "ArrayUtil.h"
+
+void dlmwrite(const std::string& outfile, const Array2Df& mat) {
+  
+}
---
+++ UILib/inc/ArrayUtil.h
@@ -0,0 +1,4 @@
+#include "Array2D.h"
+#include <string>
+
+void dlmwrite(const std::string& outfile, const Array2Df&mat);
