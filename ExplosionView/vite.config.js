9fe05d6b36a212881eda7048c9c6bdd8b91b509c
Desai Chen <desai@foundationllm.com>
2025-10-03 15:18:21 -0400

    help

--- ExplosionView/vite.config.js
+++ ExplosionView/vite.config.js
@@ -1,24 +1,27 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 //use tailwind because gemini loves it lol lol lol
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     port: 9002,
   },
-  base: '',
+  base: "",
   build: {
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        help: resolve(__dirname, "help.html"),
+      },
       output: {
-        entryFileNames: "[name].js", 
+        entryFileNames: "[name].js",
         // For asynchronously loaded chunks
-        chunkFileNames: "[name]-c.js", 
+        chunkFileNames: "[name]-c.js",
         // For other assets (images, CSS, etc.)
         assetFileNames: "assets/[name]-[extname]",
       },
     },
   },
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
 });
